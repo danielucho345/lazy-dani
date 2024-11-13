1ab4759e0c912187805e52b92396889103886eb3
Daniel Mora <danielucho345@gmail.com>
2024-11-12 21:02:00 -0600

    copilot fix

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -8,7 +8,7 @@ return {
       enabled = not vim.g.ai_cmp,
       auto_trigger = true,
       keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
+        accept = "<C-l>", -- Use a different key if <Tab> conflicts with nvim-cmp
         next = "<M-]>",
         prev = "<M-[>",
       },
@@ -17,6 +17,10 @@ return {
     filetypes = {
       markdown = true,
       help = true,
+      lua = true, -- Add other filetypes as needed
     },
   },
+  config = function(_, opts)
+    require("copilot").setup(opts)
+  end,
 }
