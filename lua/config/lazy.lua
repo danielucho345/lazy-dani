315e1e706d242c5fe88f73faa378be4f3e2f9274
Daniel Mora <danielmora@MacBook-Air-de-Daniel.local>
2024-12-11 19:06:21 -0600

    remove none-ls

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,6 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
-    { import = "lazyvim.plugins.extras.lsp.none-ls" },
     {
       "L3MON4D3/LuaSnip",
       config = function()
